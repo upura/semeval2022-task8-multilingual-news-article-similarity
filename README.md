5d9f329cee28b22bad8659fba8656d87fc793c59
upura <upura0@gmail.com>
2022-01-02 10:27:47 +0900

    Init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# SemEval 2022 Task 8: Multilingual News Article Similarity
+
+- [CodaLab](https://competitions.codalab.org/competitions/33835)
+- [downloader](https://github.com/euagendas/semeval_8_2022_ia_downloader)
---
+++ src/check_dataset.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+if __name__ == "__main__":
+    text_df = pd.read_csv("../input/semeval2022/text_dataframe.csv")
+    print(text_df.shape)
+    print(text_df.isnull().sum())
+    print(text_df.head(2))
+    print(text_df.iloc[0])
