c2c40d767950196484a91f4cda2450cd9279f3ed
upura <upura0@gmail.com>
2022-01-03 16:33:30 +0900

    Fix

--- src/check_dataset.py
+++ src/check_dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 if __name__ == "__main__":
-    text_df = pd.read_csv("../input/semeval2022/text_dataframe.csv")
+    text_df = pd.read_csv("../input/semeval2022/text_dataframe.csv", low_memory=False)
     print(text_df.shape)
     print(text_df.isnull().sum())
     print(text_df.head(2))
--- src/prepare_dataset.py
+++ src/prepare_dataset.py
@@ -7,73 +7,13 @@ from tqdm import tqdm
 
 def fetch_dateframe(text_id: str, meta):
     search_results = list(meta.glob(f"*/{text_id}.json"))
-    if len(search_results) == 0:
-        res_df = pd.DataFrame(
-            columns=[
-                "source_url",
-                "url",
-                "title",
-                "top_image",
-                "meta_img",
-                "images",
-                "movies",
-                "text",
-                "keywords",
-                "meta_keywords",
-                "tags",
-                "authors",
-                "publish_date",
-                "summary",
-                "article_html",
-                "meta_description",
-                "meta_lang",
-                "meta_favicon",
-                "canonical_link",
-                "meta_data.oath.guce.consent-host",
-                "meta_data.msapplication-TileColor",
-                "meta_data.msapplication-TileImage",
-                "meta_data.msvalidate.01",
-                "meta_data.referrer",
-                "meta_data.theme-color",
-                "meta_data.twitter.dnt",
-                "meta_data.twitter.site",
-                "meta_data.twitter.player.identifier",
-                "meta_data.twitter.player.height",
-                "meta_data.twitter.player.width",
-                "meta_data.twitter.card",
-                "meta_data.twitter.description",
-                "meta_data.twitter.image.identifier",
-                "meta_data.twitter.image.src",
-                "meta_data.twitter.title",
-                "meta_data.summary",
-                "meta_data.robots.",
-                "meta_data.news_keywords",
-                "meta_data.apple-itunes-app",
-                "meta_data.description",
-                "meta_data.og.type",
-                "meta_data.og.image",
-                "meta_data.og.description",
-                "meta_data.og.title",
-                "meta_data.og.url",
-                "meta_data.fb.pages",
-                "meta_data.al.android.app_name",
-                "meta_data.al.android.package",
-                "meta_data.al.android.url",
-                "meta_data.al.ios.app_name",
-                "meta_data.al.ios.app_store_id",
-                "meta_data.al.ios.url",
-                "text_id",
-            ]
-        )
-        return
-    else:
-        text_path = str(search_results[0])
-        with open(text_path) as f:
-            text_json = f.read()
-        text_json = json.loads(text_json)
-        res_df = pd.json_normalize(text_json)
-        res_df["text_id"] = text_id
-        return res_df
+    text_path = str(search_results[0])
+    with open(text_path) as f:
+        text_json = f.read()
+    text_json = json.loads(text_json)
+    res_df = pd.json_normalize(text_json)
+    res_df["text_id"] = text_id
+    return res_df
 
 
 if __name__ == "__main__":
@@ -83,6 +23,10 @@ if __name__ == "__main__":
 
     text_dfs = []
     for text_id in tqdm(text_ids):
-        text_dfs.append(fetch_dateframe(text_id, meta))
+        try:
+            res = fetch_dateframe(text_id, meta)
+            text_dfs.append(res)
+        except IndexError:
+            pass
     text_df = pd.concat(text_dfs).reset_index(drop=True)
     text_df.to_csv("../input/semeval2022/text_dataframe.csv", index=False)
